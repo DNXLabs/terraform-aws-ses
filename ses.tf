d5b8120d234b9eeea1d01e51f1a91e5964e9009f
Maycon Costa <maycon.costa@dnx.solutions>
2022-11-21 14:04:04 +1100

    Implement aws ses module

---
+++ ses.tf
@@ -0,0 +1,37 @@
+resource "aws_ses_domain_identity" "ses_domain" {
+  domain = var.domain
+}
+
+resource "aws_ses_domain_mail_from" "ses_domain_mail_from" {
+  domain           = aws_ses_domain_identity.ses_domain.domain
+  mail_from_domain = "mail.${aws_ses_domain_identity.ses_domain.domain}"
+}
+
+resource "aws_ses_domain_dkim" "ses_domain_dkim" {
+  domain = join("", aws_ses_domain_identity.ses_domain.*.domain)
+}
+
+resource "aws_route53_record" "amazonses_verification_record" {
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = "_amazonses.${var.domain}"
+  type    = "TXT"
+  ttl     = "600"
+  records = [join("", aws_ses_domain_identity.ses_domain.*.verification_token)]
+}
+
+resource "aws_route53_record" "amazonses_dkim_record" {
+  count   = 3
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = "${aws_ses_domain_dkim.ses_domain_dkim.dkim_tokens[count.index]}._domainkey"
+  type    = "CNAME"
+  ttl     = "600"
+  records = ["${aws_ses_domain_dkim.ses_domain_dkim.dkim_tokens[count.index]}.dkim.amazonses.com"]
+}
+
+resource "aws_route53_record" "ses_domain_mail_from_txt" {
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = aws_ses_domain_mail_from.ses_domain_mail_from.mail_from_domain
+  type    = "TXT"
+  ttl     = "600"
+  records = ["v=spf1 include:amazonses.com -all"]
+}
